dd9d957c8e3cab4e8795b9f0cb1814691450e5a9
adrien nhem <adrien.nhem@gmail.com>
2020-12-17 21:26:46 +0000

    add posts and users controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/create'
+  get 'posts/update'
+  get 'posts/destroy'
+  get 'users/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
